a13e39bc8f573673ef700bd21adcf8b464e6f001
antitititi <127745629+antitititi@users.noreply.github.com>
2023-03-13 15:47:14 +0100

    Add files via upload

---
+++ banana.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero = 42;
+    double numerolungo = 544475577;
+    float numeroconlavirgola = 5.676;
+    char carattere = 'c';
+    char c = 99;
+    printf("il numero è: %d\n", numero);
+    printf("il numero lungo è: %f, numero con la virgola è: %f\n", numerolungo, numeroconlavirgola);
+    printf("il carattere è: %c\n", carattere);
+}
